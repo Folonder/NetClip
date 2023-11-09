f124cefb34d3bf6ae8eac9c67a4984a203c88743
Folonder <olroldugin@yandex.ru>
2023-11-09 13:35:04 +0300

    initial commit

---
+++ app/infrastructure/clipboard.py
@@ -0,0 +1,10 @@
+from app.service.message_model import MessageModel
+
+
+class ClipboardTransfer:
+    def __init__(self):
+        __messages: list[MessageModel] = []
+
+    def get_messages(self) -> list[MessageModel]:
+        pass
+
