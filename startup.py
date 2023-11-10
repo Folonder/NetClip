7dd8bc34ad87da9fa7fb4c13c04c86720c0ee6a6
Folonder <74664782+Folonder@users.noreply.github.com>
2023-11-10 09:16:27 +0300

    add: create post-messages endpoint

--- startup.py
+++ startup.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 from app.infrastructure.clipboard import Clipboard
 from app.infrastructure.requests_client import RequestsClient
 from app.service.clipboard_transfer import ClipboardTransfer
-
+from app.service.message_model import MessageModel
 
 app = FastAPI()
+clipboard_transfer = ClipboardTransfer(RequestsClient(), Clipboard())
 
 
-if __name__ == "__main__":
-    clipboard_transfer = ClipboardTransfer(RequestsClient(), Clipboard())
+class MessageList(BaseModel):
+    messages: list[MessageModel]
 
+
+@app.post("/post-messages")
+def post_messages(messages: MessageList):
+    print(messages)
+    clipboard_transfer.remote_messages.extend(messages.messages)
+    return {'Ok': 'successful'}
