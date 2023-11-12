eb23550e373baa0c5819ce9b51ac82e9c8eaafe0
OkejoTM <denastytiko03@mail.ru>
2023-11-12 17:39:18 +0300

    add: paste/get messages in clipboard

--- app/infrastructure/clipboard.py
+++ app/infrastructure/clipboard.py
@@ -1,14 +1,34 @@
+from time import sleep
 from app.service.message_model import MessageModel
+from pyperclip import copy
+
+from subprocess import run
+from re import findall, DOTALL
 
 
 class Clipboard:
     def __init__(self):
         pass
 
-    def paste_messages(self):
+    def paste_messages(self, messages: list[MessageModel]):
         # TODO: paste message into local clipboard
-        pass
+        for message in messages:
+            copy(message.data)
+            sleep(0.25)
 
     def get_messages(self) -> list[MessageModel]:
         # TODO: get messages from local clipboard
-        pass
+        command = "app/other/SharpClipHistory.exe"
+
+        result = run(command, capture_output=True, text=True, encoding='cp866')
+        pattern = r'\[\+\] (.*?)(?=\[\+\]|$)'
+        matches = findall(pattern, result.stdout, DOTALL)
+        matches.pop(0)
+        matches.pop(0)
+        output_list = []
+
+        for match in matches:
+            match = match[21::].strip('\n')
+            output_list.append(MessageModel(None, None, match))
+
+        return output_list
--- startup.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-
-
-
-app = FastAPI
