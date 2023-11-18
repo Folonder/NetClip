a5226559b47039602ffb22444f79b26c81dc04de
Folonder <olroldugin@yandex.ru>
2023-11-18 20:56:30 +0300

    refactoring: add threading

--- startup.py
+++ startup.py
@@ -1,3 +1,5 @@
+import threading
+
 from fastapi import FastAPI
 from uvicorn import run
 
@@ -15,7 +17,23 @@ def post_messages(messages: MessageList):
     return {'Ok': 'successful'}
 
 
+def run_server():
+    run(application, host=get_local_ip(), port=8000)
+
+
+def run_input():
+    for i in range(100):
+        print("lol")
+
+
 if __name__ == "__main__":
+    thread1 = threading.Thread(target=run_server)
+    thread2 = threading.Thread(target=run_input)
     run_with_admin_rights("startup.py")
     run_powershell_script(r'app/other/enable_ping.PS1')
-    run(application, host=get_local_ip(), port=8000)
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
