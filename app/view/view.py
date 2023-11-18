5486b2fd25358d196651c836a0eb25ce0affbca5
Folonder <olroldugin@yandex.ru>
2023-11-18 22:04:07 +0300

    add view realization

--- app/view/view.py
+++ app/view/view.py
@@ -1,2 +1,58 @@
-class View:
-    pass
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QScrollArea, QWidget, QVBoxLayout, \
+    QLabel, QCheckBox, QHBoxLayout
+
+from app.service.clipboard_transfer import ClipboardTransfer
+
+
+class View(QMainWindow):
+    def __init__(self, clipboard_transfer: ClipboardTransfer):
+        super().__init__()
+        self.__clipboard_transfer = clipboard_transfer
+        self.__widget_messages = []
+        uic.loadUi(r"app/view/src/mainmenu.ui", self)
+        self.init_ui()
+        self.update_local_messages_view()
+
+    def init_ui(self):
+        self.create_widget_layout()
+        self.updateButton.clicked.connect(self.update_local_messages_view)
+
+    def create_widget_layout(self):
+        scroll_area = QScrollArea(self)
+        scroll_area.setWidgetResizable(True)
+
+        widget_container = QWidget(scroll_area)
+        scroll_area.setWidget(widget_container)
+
+        container_layout = QVBoxLayout(widget_container)
+        self.verticalLayout.addWidget(scroll_area)
+        return container_layout
+
+    def update_local_messages_view(self):
+        self.verticalLayout.takeAt(0).widget().deleteLater()
+        self.update_messages_widgets()
+        layout = self.create_widget_layout()
+        self.show_widget_messages(layout)
+
+    def show_widget_messages(self, layout):
+        for widget in self.__widget_messages:
+            layout.addLayout(widget)
+
+    def generate_message_widget(self, message):
+        label = QLabel(message)
+        checkbox = QCheckBox()
+
+        layout = QHBoxLayout()
+        layout.addWidget(label)
+
+        layout.addStretch(1)
+        layout.addWidget(checkbox)
+
+        return layout
+
+    def update_messages_widgets(self):
+        self.__widget_messages = []
+        for message in self.__clipboard_transfer.get_local_messages():
+            self.__widget_messages.append(self.generate_message_widget(message.data))
--- startup.py
+++ startup.py
@@ -1,11 +1,14 @@
+import sys
 import threading
 
+from PyQt5.QtWidgets import QApplication
 from fastapi import FastAPI
 from uvicorn import run
 
 from app.service.clipboard_transfer import ClipboardTransfer
 from app.service.message_model import MessageList
 from app.utils import get_local_ip, run_with_admin_rights, run_powershell_script
+from app.view.view import View
 
 application = FastAPI()
 clipboard_transfer = ClipboardTransfer()
@@ -21,14 +24,16 @@ def run_server():
     run(application, host=get_local_ip(), port=8000)
 
 
-def run_input():
-    for i in range(100):
-        print("lol")
+def run_view():
+    app = QApplication(sys.argv)
+    ex = View(clipboard_transfer)
+    ex.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
     thread1 = threading.Thread(target=run_server)
-    thread2 = threading.Thread(target=run_input)
+    thread2 = threading.Thread(target=run_view)
     run_with_admin_rights("startup.py")
     run_powershell_script(r'app/other/enable_ping.PS1')
     thread1.start()
